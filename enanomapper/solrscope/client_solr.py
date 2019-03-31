242886e01eb65c613e6eb16a04adb49ae956fbbd
n10705013 <jeliazkova.nina@gmail.com>
2019-03-31 10:13:07 +0300

    refactoring

--- enanomapper/solrscope/client_solr.py
+++ enanomapper/solrscope/client_solr.py
@@ -3,57 +3,63 @@ import pandas as pd
 
 from pandas.io.json import json_normalize
 
+       
 def post(service_uri,query,auth=None):
-    r = requests.post(service_uri+"/select",data=query, auth=auth)
+    r = requests.post(service_uri + "/select",data=query, auth=auth)
     return r
 
-def parseFacets(facets,key="ALL",prefix="",process=None):
-    count = facets['count']
-    if 'val' in facets:
-        val = facets['val']            
-    else:
-        val='ALL'
-    if process is None:    
-        print("{}'{}'\t{}".format(prefix,val,count)) 
-    else:
-        process(prefix,val,count,key)
-    if facets== None:
-        return
-    for f in facets.keys():
-        key = None
-        if 'count'==f:
-            continue
-        elif 'val'==f:
-            continue
-        else:
-            key=f
-            for bucket in facets[f]['buckets']:
-                parseFacets(bucket,key,prefix+"\t")
-                
-def getQueryFacet(query='*:*',facets=["endpointcategory_s","effectendpoint_s","unit_s"],fq=''):
-    json_facet = getNestedFacets(facets);
-    query={'q': query,'fq' : fq, "wt" : "json", "json.facet": json_facet, 'rows': 0}
-    return query
 
-
-def getFacet(field="endpointcategory_s",n=1,nested=None):
-    fieldname="field{}".format(n)
-    if nested==None:
-        nested_facet=""
-    else:    
-        nested_facet= ", facet:" + nested 
-
-    return "{" + fieldname + ": {"+ "{}:{},{}:{} ,limit : -1, mincount:1, missing:true ".format("type","terms","field",field) +nested_facet + "}}"
     
+class Facets:
 
-def getNestedFacets(facets=["endpointcategory_s","effectendpoint_s","unit_s"]):
-    n=len(facets)
-    if n==1:
-        nested_facet=None
-    else:    
-        nested_facet = getNestedFacets(facets[1:len(facets)])
-    facet = getFacet(facets[0],n,nested_facet)
-    return facet
+    def getQuery(self,query='*:*',facets=["endpointcategory_s","effectendpoint_s","unit_s"],fq=''):
+        json_facet = self.getNestedFacets(facets);
+        query={'q': query,'fq' : fq, "wt" : "json", "json.facet": json_facet, 'rows': 0}
+        return query
+
+    def parse(self,facets,key="ALL",prefix="",process=None):
+        count = facets['count']
+        if 'val' in facets:
+            val = facets['val']            
+        else:
+            val='ALL'
+        if process is None:    
+            print("{}'{}'\t{}".format(prefix,val,count)) 
+        else:
+            process(prefix,val,count,key)
+        if facets== None:
+            return
+        for f in facets.keys():
+            key = None
+            if 'count'==f:
+                continue
+            elif 'val'==f:
+                continue
+            else:
+                key=f
+                for bucket in facets[f]['buckets']:
+                    self.parse(bucket,key,prefix+"\t")
+
+    def getFacet(self,field="endpointcategory_s",n=1,nested=None):
+        fieldname="field{}".format(n)
+        if nested==None:
+            nested_facet=""
+        else:    
+            nested_facet= ", facet:" + nested 
+        return "{" + fieldname + ": {"+ "{}:{},{}:{} ,limit : -1, mincount:1, missing:true ".format("type","terms","field",field) +nested_facet + "}}"
+
+
+    def getNestedFacets(self,facets=["endpointcategory_s","effectendpoint_s","unit_s"]):
+        if facets is None:
+            return ''
+        
+        n=len(facets)
+        if n==1:
+            nested_facet=None
+        else:    
+            nested_facet = self.getNestedFacets(facets[1:len(facets)])
+        facet = self.getFacet(facets[0],n,nested_facet)
+        return facet
 
         
     #json_facet="{field1: {" + "{}:{},{}:{} ,limit : -1, mincount:1 ".format("type","terms","field",field1) + field2_facet + " }}"
@@ -61,26 +67,26 @@ def getNestedFacets(facets=["endpointcategory_s","effectendpoint_s","unit_s"]):
     #query={'q': query,"wt" : "json", "json.facet": json_facet, 'rows': 0}
     #return query
 
-def parseFacet(response_json,key1 = "field1",key2 = "field2"):
-    fields = response_json["facets"][key1]["buckets"]
-    fields_name=[]
-    fields_count=[]
-    field_2=[]
-    for value in fields:
-        fields_name.append(value['val'])
-        fields_count.append(value['count'])
-        _field2=''
-        try:
-            for item in value[key2]['buckets']:
-                if item['val'].startswith("EP_"):
-                    continue
-                _field2 = _field2 + " " + item['val']
+    def parseFacet2(self,response_json,key1 = "field1",key2 = "field2"):
+        fields = response_json["facets"][key1]["buckets"]
+        fields_name=[]
+        fields_count=[]
+        field_2=[]
+        for value in fields:
+            fields_name.append(value['val'])
+            fields_count.append(value['count'])
+            _field2=''
+            try:
+                for item in value[key2]['buckets']:
+                    if item['val'].startswith("EP_"):
+                        continue
+                    _field2 = _field2 + " " + item['val']
 
-        except Exception as err:
-            pass
-        field_2.append(_field2.strip())    
+            except Exception as err:
+                pass
+            field_2.append(_field2.strip())    
 
-    return pd.DataFrame({key1 : fields_name, "count" : fields_count, key2 : field_2})
+        return pd.DataFrame({key1 : fields_name, "count" : fields_count, key2 : field_2})
 
 def getSolrQuery(settings,service_url=None):
     
