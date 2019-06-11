0b674b49314d7b5d4d5d3b811d351ded0ac1333b
n10705013 <jeliazkova.nina@gmail.com>
2019-06-11 13:17:02 +0300

    auth refactoring and cleanup

--- enanomapper/solrscope/aa.py
+++ enanomapper/solrscope/aa.py
@@ -4,24 +4,42 @@ import pandas as pd
 import yaml
 from pandas.io.json import json_normalize
 
+def search_service_protected(url,apikey):
+    return (url,apikey)
+def search_service_open(url):
+    return (url)    
+
 class AmbitAPIKEYAuth(AuthBase):
     """Authorization: APIKEY XXXX"""
-    def __init__(self, apikey):
+    def __init__(self, apikey=None):
         self.apikey = apikey
 
     def __call__(self, r):
         r.headers['Authorization'] = "APIKEY {}".format(self.apikey)
         return r   
     
+    def setKey(self, apikey):
+        self.apikey=apikey
+         
+    
 class GraviteeAuth(AuthBase):
-    def __init__(self, apikey):
+    def __init__(self, apikey=None):
         self.apikey = apikey
 
     def __call__(self, r):
         r.headers['X-Gravitee-Api-Key'] = self.apikey
-        return r       
+        return r   
     
+    def setKey(self, apikey):
+        self.apikey=apikey    
+        
     
+def operationAuth(config,path='/select',method='get',auth="enmKeyAuth"):
+    operation = config['paths'][path]['get']['security']
+    return [apikey for item in operation if auth in item]    
+
+
+ 
 def parseOpenAPI3(url='https://search.data.enanomapper.net/api-docs/', config='solr.yaml',auth="enmKeyAuth"):
     apikey=None
     config = yaml.load(requests.get(url+config).text)
@@ -35,8 +53,16 @@ def parseOpenAPI3(url='https://search.data.enanomapper.net/api-docs/', config='s
     else:
         config_security = None
         
-    return config,config_servers, apikey
+    auth_class=None    
+    msg=""
+    if 'X-Gravitee-Api-Key' == apikey:
+        auth_class = GraviteeAuth()
+        msg = 'Enter `{}` you have received upon subscription to http://api.ideaconsult.net'.format(apikey) 
+        
+    elif 'Authorization' == apikey:
+        auth_class = AmbitAPIKEYAuth()
+        msg = 'Enter AMBIT API Key'
     
-def operationAuth(config,path='/select',method='get',auth="enmKeyAuth"):
-    operation = config['paths'][path]['get']['security']
-    return [apikey for item in operation if auth in item]    
+    return config,config_servers, apikey, auth_class, msg
+
+        
