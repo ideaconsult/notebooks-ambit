8753aadd75116b81d51e2bb4e508a30467331923
vedina <jeliazkova.nina@gmail.com>
2022-01-20 12:45:58 +0200

    match parameters task

--- enanomapper/pipelines/curation/tasks/match_params.py
+++ enanomapper/pipelines/curation/tasks/match_params.py
@@ -1,21 +1,23 @@
 # + tags=["parameters"]
-upstream = ["retrieve","retrieve_templates"]
+upstream = ["retrieve"]
 folder_output = None
 # -
 
-import requests
-import json
+
 import os,os.path
 import pandas as pd
 
-try:
-    with open(os.path.join(folder_output,"templates","pchem.json"), 'r',encoding="utf-8") as file:
-        template = json.load(file)
-
-    print(template)
-except Exception as err:
-    print(err)
 
 params = pd.read_csv(os.path.join(folder_output,"params.txt"),sep="\t",encoding="utf-8")
 
-params
+from thefuzz import process
+
+prms =params["field_clean"].unique()
+tmp = []
+for prm in prms:
+    _m = process.extract(prm, prms, limit=5)
+    for m in _m:
+        tmp.append([prm,m[0],m[1]])
+    
+df = pd.DataFrame(tmp,columns=["field_clean","match","score"])
+df.to_csv(os.path.join(folder_output,"terms","matched_params.txt",sep="\t"),index=False)    
