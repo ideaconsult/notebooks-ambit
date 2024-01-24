02382ee4aadf0b410500706889f7cbaf9daaef4e
vedina <jeliazkova.nina@gmail.com>
2024-01-24 14:52:44 +0200

    update

--- enanomapper/pipelines/nexus/tasks/extract.py
+++ enanomapper/pipelines/nexus/tasks/extract.py
@@ -16,24 +16,29 @@ import  pynanomapper.datamodel.ambit as m2n
 from pynanomapper.datamodel.nexus_writer import to_nexus
 import nexusformat.nexus.tree as nx
 import traceback
+import json
 
 def json2nexus(url_db,auth,pjson):
     substances = m2n.Substances(**pjson)
     for substance in substances.substance:
         try:
-            print(substance)
+            sjson = None
             url = "{}/substance/{}/study&media=application/json&max={}".format(url_db,substance.i5uuid,10000)
             #url = "{}/study?media=application/json&max=10000".format(substance.URI)
             response = requests.get(url,auth=auth)
             if response.status_code ==200:
                 print(substance.i5uuid)
                 sjson = response.json()
-                print(sjson)
                 substance.study = m2n.Study(**sjson).study
             else:
                 print(response.status_code,url)
         except Exception as err:    
-            print(url,err)
+            print("An exception occurred: %s", str(err))
+            print("Exception traceback:\n%s", traceback.format_exc())
+            # Write JSON data to the file
+            with open(os.path.join(folder_output,"{}.json".format(substance.i5uuid)), 'w') as json_file:
+                json.dump(sjson, json_file, indent=2)  
+            break
  
     return substances
 
@@ -45,16 +50,28 @@ if auth_object!=None:
 rows = 1000
 url_db = "{}/enm/{}/substance?{}&media=application/json&max={}".format(enm_api_url,db,query,rows)
 
+OK = False
 response = requests.get(url_db,auth=auth_object)
 if response.status_code ==200:
     #print(response.text)
     pjson = response.json()
-    print(pjson)
-    nxroot = nx.NXroot()
-    substances = json2nexus("{}/enm/{}".format(enm_api_url,db),auth_object,pjson)
-    substances.to_nexus(nxroot)
-    nxroot.save(os.path.join(folder_output,"substances.nxs"),mode="w")
+    OK = True
 else:
     print(response.status_code)
+
+
+if OK:
+    OK = False
+    try:
+        substances = json2nexus("{}/enm/{}".format(enm_api_url,db),auth_object,pjson)
+        OK = True
+    except Exception as err:  
+        print(err)
+
+if OK:
+    nxroot = nx.NXroot()
+    substances.to_nexus(nxroot)
+    nxroot.save(os.path.join(folder_output,"substances.nxs"),mode="w")
+
 
     
